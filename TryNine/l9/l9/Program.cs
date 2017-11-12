d177c02f9252f40accde7cfa1473622d79a5c04f
JollyGolf <kybcemwesley@gmail.com>
2017-11-12 13:28:50 +0200

    add practice work 9 (1/2)

---
+++ TryNine/l9/l9/Program.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Никита
+ * Date: 10.10.2017
+ * Time: 10:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace interf1
+{
+     interface Scope
+        {
+            double scope(); 
+        }
+     interface Area
+        {
+            double area(); 
+        }
+//1
+class A: Scope
+    {
+	double a, b, h;
+        public A()
+        {
+        }
+        public A(double aa, double bb, double hh)
+        {
+            a=aa;
+            b=bb;
+            h=hh;
+        }
+        public double scope()
+        {                        
+            return (a*b*h);
+        }        
+    }
+//2
+class B: Area
+    {
+	double c, d;
+        public B()
+        {
+        }
+        public B(double cc, double dd)
+        {
+            c=cc;
+            d=dd;
+        }
+        public double area()
+        {                        
+            return (c*d);
+        }        
+    }
+class Program
+    {        
+        public static void Main(string[] args)
+        {
+            double a, b, h;
+            Console.WriteLine("Enter a=");
+            a=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter b=");
+            b=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter h=");
+            h=Convert.ToDouble(Console.ReadLine());
+            A ob=new A(a,b,h);
+            double sScope;
+            sScope=ob.scope();
+            Console.WriteLine("Scope = "+sScope);
+            double c, d;
+            Console.WriteLine("Enter c=");
+            c=Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter d=");
+            d=Convert.ToDouble(Console.ReadLine());
+            B ob2=new B(c,d);
+            double sArea;
+            sArea=ob2.area();
+            Console.WriteLine("Area = "+sArea);         
+            Console.Write("Press key to continue");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ TryNine/l9_2/l9_2/Program.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Никита
+ * Date: 10.10.2017
+ * Time: 10:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace ArrayList_studet
+{
+	public class Student : IComparable, IComparer
+	{
+		string name;
+		double mark;
+		public Student()
+		{
+			name = "NoName";
+			mark = 0;
+		}
+		public Student(string fio, double m)
+		{
+			name = fio;
+			mark = m;
+		}
+		public void output()
+		{
+			Console.WriteLine("name is {0} mark = (1)", name, mark);
+		}
+		public int Compare(Object x0, Object y0)
+		{
+			Student x = (Student)x0;
+			Student y = (Student)y0;
+			if (String.Compare(x.name, y.name) < 0)
+			{
+				return -1;
+			}
+			else if (String.Compare(x.name, y.name) == 0) 
+			{
+				if(x.mark < y.mark)
+				{
+					return -1; 
+				}
+				else if(x.mark == y.mark)
+				{
+					return 0; 
+				}
+				else 
+				{ 
+					return 1;
+				}
+			}
+			else 
+			{
+				return 1;
+			}
+		}
+			public int CompareTo(Object x)
+			{
+				return Compare(this,x);
+			}
+		}
+		class Program
+		{
+			public static void Main(string[] args)
+			{
+				Console.Write("Test student");
+				Console.Write("\nList student before sort");
+				ArrayList x = new ArrayList();
+				x.Add(new Student("Nick", 3.5));
+				x.Add(new Student("Artem", 2));
+				x.Add(new Student("Asya", 4));
+				x.Add(new Student("Inna", 2.6));
+				x.Add(new Student("Pavel", 1));
+				Student st = new Student();
+				for (int i=0; i < x.Count; i++)
+				{
+					st= (Student)x[i];
+					st.output();
+				}
+				IComparer Comp = new Student();
+				x.Sort(Comp);
+				Console.Write("List after sort");
+				for (int i=0; i < x.Count; i++)
+				{
+					st= (Student)x[i];
+					st.output();
+				}
+			}
+		}
+	}
